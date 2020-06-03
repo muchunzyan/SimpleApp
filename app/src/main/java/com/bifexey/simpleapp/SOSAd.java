7f1137b04583f9bc5d455d17f21a0e6b81890c5b
Uchunzhyan Mikhail <msuchunzhyan@edu.hse.ru>
2020-06-03 14:39:35 +0300

    SOSFragment, SOSAd

---
+++ app/src/main/java/com/bifexey/simpleapp/SOSAd.java
@@ -0,0 +1,170 @@
+package com.bifexey.simpleapp;
+
+public class SOSAd {
+    private String SOS_first_name;
+    private String SOS_second_name;
+    private String SOS_location;
+    private String SOS_subjectFromSpinner;
+    private String SOS_locationFromSpinner;
+    private String SOS_subjectName;
+    private String SOS_subjectTopic;
+    private String SOS_year;
+    private String SOS_month;
+    private String SOS_day;
+    private String SOS_hour;
+    private String SOS_minute;
+    private String SOS_price;
+    private String SOS_comment;
+    private String SOS_phone_number;
+    private String SOS_key;
+
+    public SOSAd(String SOS_first_name, String SOS_second_name, String SOS_location, String SOS_subjectFromSpinner, String SOS_locationFromSpinner,  String SOS_subjectName, String SOS_subjectTopic, String SOS_year, String SOS_month, String SOS_day, String SOS_hour, String SOS_minute, String SOS_price, String SOS_comment, String SOS_phone_number, String SOS_key) {
+        this.SOS_first_name = SOS_first_name;
+        this.SOS_second_name = SOS_second_name;
+        this.SOS_location = SOS_location;
+        this.SOS_subjectFromSpinner = SOS_subjectFromSpinner;
+        this.SOS_locationFromSpinner = SOS_locationFromSpinner;
+        this.SOS_subjectName = SOS_subjectName;
+        this.SOS_subjectTopic = SOS_subjectTopic;
+        this.SOS_year = SOS_year;
+        this.SOS_month = SOS_month;
+        this.SOS_day = SOS_day;
+        this.SOS_hour = SOS_hour;
+        this.SOS_minute = SOS_minute;
+        this.SOS_price = SOS_price;
+        this.SOS_comment = SOS_comment;
+        this.SOS_phone_number = SOS_phone_number;
+        this.SOS_key = SOS_key;
+    }
+
+    public SOSAd() {
+    }
+
+    public String getSOS_first_name() {
+        return SOS_first_name;
+    }
+
+    public void setSOS_first_name(String SOS_first_name) {
+        this.SOS_first_name = SOS_first_name;
+    }
+
+    public String getSOS_second_name() {
+        return SOS_second_name;
+    }
+
+    public void setSOS_second_name(String SOS_second_name) {
+        this.SOS_second_name = SOS_second_name;
+    }
+
+    public String getSOS_location() {
+        return SOS_location;
+    }
+
+    public void setSOS_location(String SOS_location) {
+        this.SOS_location = SOS_location;
+    }
+
+    public String getSOS_subjectFromSpinner() {
+        return SOS_subjectFromSpinner;
+    }
+
+    public void setSOS_subjectFromSpinner(String SOS_subjectFromSpinner) {
+        this.SOS_subjectFromSpinner = SOS_subjectFromSpinner;
+    }
+
+    public String getSOS_locationFromSpinner() {
+        return SOS_locationFromSpinner;
+    }
+
+    public void setSOS_locationFromSpinner(String SOS_locationFromSpinner) {
+        this.SOS_locationFromSpinner = SOS_locationFromSpinner;
+    }
+
+    public String getSOS_subjectName() {
+        return SOS_subjectName;
+    }
+
+    public void setSOS_subjectName(String SOS_subjectName) {
+        this.SOS_subjectName = SOS_subjectName;
+    }
+
+    public String getSOS_subjectTopic() {
+        return SOS_subjectTopic;
+    }
+
+    public void setSOS_subjectTopic(String SOS_subjectTopic) {
+        this.SOS_subjectTopic = SOS_subjectTopic;
+    }
+
+    public String getSOS_year() {
+        return SOS_year;
+    }
+
+    public void setSOS_year(String SOS_year) {
+        this.SOS_year = SOS_year;
+    }
+
+    public String getSOS_month() {
+        return SOS_month;
+    }
+
+    public void setSOS_month(String SOS_month) {
+        this.SOS_month = SOS_month;
+    }
+
+    public String getSOS_day() {
+        return SOS_day;
+    }
+
+    public void setSOS_day(String SOS_day) {
+        this.SOS_day = SOS_day;
+    }
+
+    public String getSOS_hour() {
+        return SOS_hour;
+    }
+
+    public void setSOS_hour(String SOS_hour) {
+        this.SOS_hour = SOS_hour;
+    }
+
+    public String getSOS_minute() {
+        return SOS_minute;
+    }
+
+    public void setSOS_minute(String SOS_minute) {
+        this.SOS_minute = SOS_minute;
+    }
+
+    public String getSOS_price() {
+        return SOS_price;
+    }
+
+    public void setSOS_price(String SOS_price) {
+        this.SOS_price = SOS_price;
+    }
+
+    public String getSOS_comment() {
+        return SOS_comment;
+    }
+
+    public void setSOS_comment(String SOS_comment) {
+        this.SOS_comment = SOS_comment;
+    }
+
+    public String getSOS_phone_number() {
+        return SOS_phone_number;
+    }
+
+    public void setSOS_phone_number(String SOS_phone_number) {
+        this.SOS_phone_number = SOS_phone_number;
+    }
+
+    public String getSOS_key() {
+        return SOS_key;
+    }
+
+    public void setSOS_key(String SOS_key) {
+        this.SOS_key = SOS_key;
+    }
+}
---
+++ app/src/main/java/com/bifexey/simpleapp/ui/SOS/SOS_Fragment.java
@@ -0,0 +1,312 @@
+package com.bifexey.simpleapp.ui.SOS;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.DialogInterface.OnClickListener;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.bifexey.simpleapp.R;
+import com.bifexey.simpleapp.SOSAd;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SOS_Fragment extends Fragment implements View.OnClickListener{
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+    DatabaseReference myRefUsers = myRef.child("users");
+    DatabaseReference myRefSOSAds = myRef.child("AllSOSAds");
+    DatabaseReference myRefCurrentUser = myRefUsers.child(user.getPhoneNumber());
+    DatabaseReference myRefCurrentUserSOSAds = myRefCurrentUser.child("SOSAds");
+
+    DatabaseReference myRefCurrentUserSOSAdsDelete;
+    StorageReference mStorageRef;
+
+    FirebaseListAdapter adapter;
+    AlertDialog.Builder dialogWindow;
+
+    FloatingActionButton btn_create_SOS;
+    ImageView SOS_question_btn;
+    ListView SOS_F_list_view;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_sos_, null);
+
+//        btn_create_SOS = v.findViewById(R.id.btn_create_SOS);
+//        SOS_F_list_view = v.findViewById(R.id.SOS_F_list_view);
+//        SOS_question_btn = v.findViewById(R.id.SOS_question_btn);
+
+        btn_create_SOS.setOnClickListener(this);
+        SOS_question_btn.setOnClickListener(this);
+
+        displayAds();
+        checkOldData();
+
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+//        switch (v.getId()){
+//            case R.id.btn_create_SOS:
+//                Intent intent = new Intent(getContext(), CreateSOS.class);
+//                startActivity(intent);
+//                break;
+//            case R.id.SOS_question_btn:
+//                createDialogWindow();
+//                dialogWindow.show();
+//                break;
+//        }
+    }
+
+    private void displayAds(){
+        FirebaseListOptions<SOSAd> options = new FirebaseListOptions.Builder<SOSAd>()
+                .setQuery(myRefCurrentUserSOSAds, SOSAd.class)
+                .setLayout(R.layout.my_sos_ad_item)
+                .build();
+
+        adapter = new FirebaseListAdapter<SOSAd>(options){
+            @Override
+            protected void populateView(View v, SOSAd model, int position) {
+//                ImageView my_SOS_image = v.findViewById(R.id.my_SOS_image);
+//                TextView my_SOS_subjectFromSpinner = v.findViewById(R.id.my_SOS_subjectFromSpinner);
+//                TextView my_SOS_locationFromSpinner = v.findViewById(R.id.my_SOS_locationFromSpinner);
+//                TextView my_SOS_subjectName = v.findViewById(R.id.my_SOS_subjectName);
+//                TextView my_SOS_subjectTopic = v.findViewById(R.id.my_SOS_subjectTopic);
+//                TextView my_SOS_date_time = v.findViewById(R.id.my_SOS_date_time);
+//                TextView my_SOS_price = v.findViewById(R.id.my_SOS_price);
+//                TextView my_SOS_key = v.findViewById(R.id.my_SOS_key);
+//
+//                downloadImage(my_SOS_image);
+//                my_SOS_subjectFromSpinner.setText(model.getSOS_subjectFromSpinner());
+//                my_SOS_locationFromSpinner.setText(model.getSOS_locationFromSpinner());
+//                my_SOS_subjectName.setText(model.getSOS_subjectName());
+//                my_SOS_subjectTopic.setText(model.getSOS_subjectTopic());
+//                my_SOS_date_time.setText(model.getSOS_day() + "/" + model.getSOS_month() + "/" +
+//                        model.getSOS_year() + " " + model.getSOS_hour() + ":" + model.getSOS_minute());
+//                my_SOS_price.setText("\u20BD" + model.getSOS_price());
+//                my_SOS_key.setText(model.getSOS_key());
+            }
+        };
+
+        SOS_F_list_view.setAdapter(adapter);
+
+        View footer = new View(getActivity());
+        footer.setLayoutParams( new AbsListView.LayoutParams( ViewGroup.LayoutParams.FILL_PARENT, 180 ));
+        SOS_F_list_view.addFooterView(footer, null, false);
+
+        SOS_F_list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                RelativeLayout relativeLayout = (RelativeLayout) view;
+                String key = "Null";
+
+                View child = relativeLayout.getChildAt(relativeLayout.getChildCount() - 1);
+                if (child instanceof TextView) {
+                    key = ((TextView) child).getText().toString();
+                }
+
+//                Intent intent = new Intent(getActivity(), EditSOS.class);
+//                intent.putExtra("key", key);
+//                startActivity(intent);
+            }
+        });
+    }
+
+    private static String removeCharAt(String s, int pos) {
+        return s.substring(0, pos) + s.substring(pos + 1);
+    }
+
+    private void downloadImage(final ImageView img) {
+        mStorageRef = FirebaseStorage.getInstance().getReference("Images/");
+        StorageReference refUserIm = mStorageRef.child(removeCharAt(user.getPhoneNumber(), 0));
+
+        refUserIm.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).fit().centerCrop().into(img);
+            }
+        });
+    }
+
+    private void createDialogWindow(){
+        String title = "Что такое SOS?\n" +
+                "Как это работает?";
+        String message = "В разделе \"SOS\" вы можете разместить заявку о быстрой помощи. " +
+                "Данный вид заявок используется для получения быстрой, единовременной помощи. " +
+                "Например, вы не поняли тему на лекции или семинаре. " +
+                "Разместите заявку! И кто-то из зарегистрированных репетиторов вам ответит. " +
+                "Вы можете договориться о встрече в любое удобное для Вас время. " +
+                "Цену пользователь назначает сам.\n\n" +
+                "Для того, чтобы подробнее ознакомится с функционалом приложения, перейдите в раздел FAQ";
+        String button1String = "Закрыть";
+
+        Context context = getActivity();
+        dialogWindow = new AlertDialog.Builder(context);
+
+        dialogWindow.setTitle(title);
+        dialogWindow.setMessage(message);
+
+        dialogWindow.setPositiveButton(button1String, new OnClickListener() {
+            public void onClick(DialogInterface dialog, int arg1) {}
+        });
+        dialogWindow.setCancelable(true);
+        dialogWindow.setOnCancelListener(new OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {}
+        });
+    }
+
+    private void checkOldData(){
+        final String[] key = new String[1];
+        final int[] year = new int[1];
+        final int[] month = new int[1];
+        final int[] day = new int[1];
+        final int[] hour = new int[1];
+        final int[] minute = new int[1];
+
+        myRefSOSAds.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    for (DataSnapshot dss : ds.getChildren()) {
+                        switch (dss.getKey()) {
+                            case "sos_year":
+                                year[0] = Integer.parseInt(dss.getValue().toString());
+                                break;
+                            case "sos_month":
+                                month[0] = Integer.parseInt(dss.getValue().toString());
+                                break;
+                            case "sos_day":
+                                day[0] = Integer.parseInt(dss.getValue().toString());
+                                break;
+                            case "sos_hour":
+                                hour[0] = Integer.parseInt(dss.getValue().toString());
+                                break;
+                            case "sos_minute":
+                                minute[0] = Integer.parseInt(dss.getValue().toString());
+                                break;
+                        }
+                    }
+
+                    String date = day[0] + "." + month[0] + "." + year[0] + " " + hour[0] + ":" + minute[0];
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy hh:mm");
+                    try {
+                        Date parse = dateFormat.parse(date);
+                        long timeCurr = System.currentTimeMillis() - parse.getTime();
+                        if (timeCurr >= 0) {
+                            key[0] = ds.getKey();
+
+                            myRefCurrentUserSOSAdsDelete = myRefCurrentUserSOSAds.child(key[0]);
+                            myRefCurrentUserSOSAdsDelete.removeValue();
+
+                            myRefCurrentUserSOSAdsDelete = myRef.child("AllSOSAds").child(key[0]);
+                            myRefCurrentUserSOSAdsDelete.removeValue();
+                        }
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        myRefCurrentUserSOSAds.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    for (DataSnapshot dss : ds.getChildren()) {
+                        switch (dss.getKey()) {
+                            case "sos_year":
+                                year[0] = Integer.parseInt(dss.getValue().toString());
+                                break;
+                            case "sos_month":
+                                month[0] = Integer.parseInt(dss.getValue().toString());
+                                break;
+                            case "sos_day":
+                                day[0] = Integer.parseInt(dss.getValue().toString());
+                                break;
+                            case "sos_hour":
+                                hour[0] = Integer.parseInt(dss.getValue().toString());
+                                break;
+                            case "sos_minute":
+                                minute[0] = Integer.parseInt(dss.getValue().toString());
+                                break;
+                        }
+                    }
+
+                    String date = day[0] + "." + month[0] + "." + year[0] + " " + hour[0] + ":" + minute[0];
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy hh:mm");
+                    try {
+                        Date parse = dateFormat.parse(date);
+                        long timeCurr = System.currentTimeMillis() - parse.getTime();
+                        if (timeCurr >= 0) {
+                            key[0] = ds.getKey();
+
+                            myRefCurrentUserSOSAdsDelete = myRefCurrentUserSOSAds.child(key[0]);
+                            myRefCurrentUserSOSAdsDelete.removeValue();
+
+                            myRefCurrentUserSOSAdsDelete = myRef.child("AllSOSAds").child(key[0]);
+                            myRefCurrentUserSOSAdsDelete.removeValue();
+                        }
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+}
